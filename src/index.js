a866c3e856c3e87228f16100fa31e3a55b3b2c54
Valter junior <bloodsportjr@gmail.com>
2017-10-03 15:24:35 -0300

    feat(layout): Add ant framework
    Add ant framework liband en language config
    
    Resolves #27

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <LocaleProvider locale={enUS}>
+    <App />
+  </LocaleProvider>,
+  document.getElementById('root'));
 registerServiceWorker();
