6c602b685794da5f71050c86c5419e61e3ec9560
Jefferson Matheus <jefferson227@gmail.com>
2017-12-29 15:13:33 -0300

    refactor(employee-card): move style to a css file

--- src/components/employeeCard/EmployeeCard.js
+++ src/components/employeeCard/EmployeeCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Avatar } from 'antd';
+import './EmployeeCard.css';
 
 const { Meta } = Card;
 
@@ -8,42 +9,29 @@ const { Meta } = Card;
 const src2 = `https://image.tmdb.org/t/p/w45/
 iiBJCkVVLHAUBW6vbUhJ3RtxlXv.jpg`;
 
-const badgeStyle = {
-  padding: 8,
-  borderRadius: 14,
-  background: '#eee',
-  fontSize: 11,
-  marginRight: 10,
-};
-
-const avatarStyle = {
-  width: 45,
-  height: 68,
-};
-
 /* eslint arrow-body-style: ["error", "always"] */
 /* eslint-env es6 */
 const EmployeeCard = () => {
   return (
-    <Card style={{ width: 300 }}>
+    <Card className="employee-card">
       <Meta
         avatar={
           <Avatar
             src={src2}
             shape="square"
-            style={avatarStyle}
+            className="avatar"
             size="large"
           />
         }
         title="Daisy Ridley"
         description="Dev 3"
       />
-      <div style={{ marginTop: 21 }}>Devry USA</div>
-      <div style={{ marginTop: 21 }}>
-        <span style={badgeStyle}>Angular 2</span>
-        <span style={badgeStyle}>React</span>
-        <span style={badgeStyle}>Redux</span>
-        <span style={badgeStyle}>Java</span>
+      <div className="info">Devry USA</div>
+      <div className="info">
+        <span className="badge">Angular 2</span>
+        <span className="badge">React</span>
+        <span className="badge">Redux</span>
+        <span className="badge">Java</span>
       </div>
     </Card>
   );
