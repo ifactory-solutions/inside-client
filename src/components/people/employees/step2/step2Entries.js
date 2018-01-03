53c4f490882bcb22c5f6cb3aa3eb7ba736a17009
Francisco Souza <francisceioseph@gmail.com>
2018-01-03 16:59:45 -0300

    feat(layout): added the step 2 form for the new emplyee register module

---
+++ src/components/people/employees/step2/step2Entries.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Input } from 'antd';
+
+import { PLACEHOLDERS } from './step2Constants';
+
+export function getLine1Input() {
+  return <Input id="line_1" type="text" placeholder={PLACEHOLDERS.LINE_1} />;
+}
+
+export function getLine2Input() {
+  return <Input id="line_2" type="text" placeholder={PLACEHOLDERS.LINE_2} />;
+}
+
+export function getLine3Input() {
+  return <Input id="line_3" type="text" placeholder={PLACEHOLDERS.LINE_3} />;
+}
+
+export function getLine4Input() {
+  return <Input id="line_4" type="text" placeholder={PLACEHOLDERS.LINE_4} />;
+}
+
+export function getZipCodeInput() {
+  return (
+    <Input
+      id="zip_code"
+      type="text"
+      pattern="[0-9]{5}"
+      placeholder={PLACEHOLDERS.ZIP_CODE}
+    />
+  );
+}
+
+export function getCityInput() {
+  return <Input id="city" type="text" placeholder={PLACEHOLDERS.CITY} />;
+}
+
+export function getStateInput() {
+  return <Input id="state" type="text" placeholder={PLACEHOLDERS.STATE} />;
+}
+
+export function getCountryInput() {
+  return <Input id="country" type="text" placeholder={PLACEHOLDERS.COUNTRY} />;
+}
---
+++ src/components/people/employees/step2/step2Form.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { Form } from 'antd';
+import {
+  getLine1Input,
+  getLine2Input,
+  getLine3Input,
+  getLine4Input,
+  getZipCodeInput,
+  getCityInput,
+  getStateInput,
+  getCountryInput,
+} from './step2Entries';
+
+import { getDecoratorManager } from './step2Decorators';
+import { LABELS } from './step2Constants';
+import { FORM_ITEM_LAYOUT, HORIZONTAL_FORM_LAYOUT } from '../stepFormConstants';
+
+const FormItem = Form.Item;
+
+class NewEmployeeStep2Form extends Component {
+  constructor(props) {
+    super(props);
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    return this;
+  }
+
+  render() {
+    const { getFieldDecorator: fieldDecorator } = this.props.form; //eslint-disable-line
+    const decoratorManager = getDecoratorManager(fieldDecorator);
+
+    return (
+      <Form layout={HORIZONTAL_FORM_LAYOUT} onSubmit={this.handleOnSubmit}>
+        <FormItem {...FORM_ITEM_LAYOUT} label={LABELS.LINE_1} hasFeedback>
+          {decoratorManager.line1Decorator(getLine1Input())}
+        </FormItem>
+
+        <FormItem {...FORM_ITEM_LAYOUT} label={LABELS.LINE_2} hasFeedback>
+          {decoratorManager.line2Decorator(getLine2Input())}
+        </FormItem>
+
+        <FormItem {...FORM_ITEM_LAYOUT} label={LABELS.LINE_3} hasFeedback>
+          {getLine3Input()}
+        </FormItem>
+
+        <FormItem {...FORM_ITEM_LAYOUT} label={LABELS.LINE_4} hasFeedback>
+          {decoratorManager.line4Decorator(getLine4Input())}
+        </FormItem>
+
+        <FormItem {...FORM_ITEM_LAYOUT} label={LABELS.ZIP_CODE} hasFeedback>
+          {decoratorManager.zipCodeDecorator(getZipCodeInput())}
+        </FormItem>
+
+        <FormItem {...FORM_ITEM_LAYOUT} label={LABELS.CITY} hasFeedback>
+          {decoratorManager.cityDecorator(getCityInput())}
+        </FormItem>
+
+        <FormItem {...FORM_ITEM_LAYOUT} label={LABELS.STATE} hasFeedback>
+          {decoratorManager.stateDecorator(getStateInput())}
+        </FormItem>
+
+        <FormItem {...FORM_ITEM_LAYOUT} label={LABELS.COUNTRY} hasFeedback>
+          {decoratorManager.countryDecorator(getCountryInput())}
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+export default Form.create()(NewEmployeeStep2Form);
