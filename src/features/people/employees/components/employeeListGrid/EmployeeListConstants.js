76f7149c17c46d2a9ed75441c7c81d8869c1f667
Francisco Souza <francisceioseph@gmail.com>
2018-01-15 15:32:00 -0300

    fix(employee_list): Added some css to fix flex problems in chrome

--- src/features/people/employees/components/employeeListGrid/EmployeeListConstants.js
+++ src/features/people/employees/components/employeeListGrid/EmployeeListConstants.js
@@ -3,7 +3,7 @@ export const ROW_GUTTER = { xs: 8, sm: 16, md: 24 };
 export const CARD_LAYOUT = {
   xs: { span: 20 },
   sm: { span: 20 },
-  md: { span: 20 },
+  md: { span: 11 },
   lg: { span: 8 },
   xl: { span: 8 },
   xxl: { span: 8 },
