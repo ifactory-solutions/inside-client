d299e894ba74c23cead08e6ee59ade722cf0c764
Gustavo Barbosa <guspb94@gmail.com>
2018-03-28 11:19:39 -0300

    feat(svg_support): Show EmployeeEmptyListMsg Show EmployeeEmptyListMsg when EmployessListGrid is empty

--- src/features/people/employees/components/employeeListGrid/EmployeeListGrid.js
+++ src/features/people/employees/components/employeeListGrid/EmployeeListGrid.js
@@ -3,16 +3,20 @@ import { Row, Col } from 'antd';
 import PropTypes from 'prop-types';
 
 import EmployeeCard from '../../../../../components/employeeCard/EmployeeCard';
-
 import { ROW_GUTTER, CARD_LAYOUT } from './EmployeeListConstants';
+import EmployeeEmptyListMsg from '../employeeEmptyListMsg/EmployeeEmptyListMsg';
 
 const EmployeeListGrid = props => (
-  <Row type="flex" justify="start" gutter={ROW_GUTTER}>
-    {props.data.map(user => (
-      <Col {...CARD_LAYOUT} key={user.id} style={{ paddingBottom: '12px' }}>
-        <EmployeeCard user={user} />
-      </Col>
-    ))}
+  <Row type="flex" justify="center" gutter={ROW_GUTTER}>
+    {props.data.length ?
+      props.data.map(user => (
+        <Col {...CARD_LAYOUT} key={user.id} style={{ paddingBottom: '12px' }}>
+          <EmployeeCard user={user} />
+        </Col>
+      ))
+      :
+      <EmployeeEmptyListMsg />
+    }
   </Row>
 );
 
